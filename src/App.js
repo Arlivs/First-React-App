ee3cee6c3218e3c14bac85ad552c9c4b45f0ff8d
Arlivs <ilyayev00@bk.ru>
2021-08-22 14:09:39 +0300

    Increm-Dicrem

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import React from "react"
+import React, { useState } from "react"
 import styles from "./index.css"
 
-
-let count = 0
 function App() {
+  let [count, setNum] = useState("0")
   return (
     <div className="app">
       <p className="paragraf">Обычный текст</p>
-      <button onClick={incr} className="butn1">Минус</button>
-      <button onClick={dicr} className="butn2">Плюс</button>
+      <button onClick={()=> setNum(--count)} className="butn1">Минус</button>
+      <button onClick={()=> setNum(++count)} className="butn2">Плюс</button>
       <div className="paragraf">{count}</div>
     </div>
   )
 }
-const incr = () =>count++
-const dicr = () =>count--
 
 export default App;
